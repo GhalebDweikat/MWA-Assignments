7ec57f81aa04b6c9c56b44474b39406231594da1
GhalebDweikat <ghalebdweikat@gmail.com>
2018-12-03 10:04:51 -0600

    Fix another bug

--- W1D6/index.js
+++ W1D6/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan')
 const expressValidator = require('express-validator');
+const cors = require('cors')
 
 const app = express();
 
-app.use(morgan('combined'))
+app.use(cors());
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(expressValidator());
