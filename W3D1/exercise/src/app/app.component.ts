fcf61965ad7fb8daa74e1ee5d0dd7e9efb2342ed
GhalebDweikat <ghalebdweikat@gmail.com>
2018-12-11 09:46:23 -0600

    Fixed a small bug

--- W3D1/exercise/src/app/app.component.ts
+++ W3D1/exercise/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<app-counter counterVal = "100" (counterChange)="cntCng($event)"></app-counter>`,
+  template: `<app-counter counterVal = "100" (counterChange)="cntCng($event)"></app-counter> ComponentCounterValue={{ComponentCounterValue}}`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'exercise';
-  
+  ComponentCounterValue = 0;
   cntCng(e) {
+    this.ComponentCounterValue = e;
     console.log(e);
   }
 }
